83a2e97764a661b14982c3977727171396c61295
Andrew Hildebrand <69697528+endangeredtoast@users.noreply.github.com>
2023-05-02 14:02:22 -0500

    Update install-php-extensions.sh

--- install-php-extensions.sh
+++ install-php-extensions.sh
@@ -1,4 +1,4 @@
-yum install ImageMagick ImageMagick-devel ImageMagick-perl libsmbclient-devel
+yum install ImageMagick ImageMagick-devel ImageMagick-perl libsmbclient-devel php-gmp php-bcmath
 pecl install imagick redis smbclient acpu
 echo "extension=imagick.so" >> /etc/php.ini
 echo "extension=redis.so" >> /etc/php.ini
